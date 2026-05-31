e680788ffc2672227d4ac94070d4a33690125a19
Kaluri Himabindhu <129114144+Hiomio@users.noreply.github.com>
2026-05-31 21:49:49 +0530

    Time: 63 ms (94.82%) | Memory: 34.1 MB (30.13%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -0,0 +1,15 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+
+        asteroids.sort()
+
+        current_mass = mass
+
+        for asteroid in asteroids:
+
+            if current_mass < asteroid:
+                return False
+
+            current_mass += asteroid
+
+        return True
